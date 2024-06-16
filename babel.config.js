1bb92a0b5e6ad315ce50eef8d8cffd37c7dcbd68
pedro-olavo <polavo00@gmail.com>
2024-06-16 01:26:32 -0300

    build: adds libraries and required configuration files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@routes': './src/routes',
+          '@stores': './src/stores',
+          '@hooks': './src/hooks',
+          '@theme': './src/theme',
+        },
+      },
+    ],
+  ],
 };
