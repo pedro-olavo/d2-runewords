0b5e5cb6913a3b0118e2b70471bd50735eec82ec
pedro-olavo <polavo00@gmail.com>
2024-06-17 13:51:38 -0300

    feat: adds ListRunes screen

--- src/screens/ListRunes/ListRunes.styles.ts
+++ src/screens/ListRunes/ListRunes.styles.ts
@@ -1,6 +1,43 @@
+import { IRunewords } from '@stores/slices/types';
+import { FlatList, FlatListProps } from 'react-native';
 import styled from 'styled-components/native';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: ${({ theme }) => theme.colors['background-01']};
+`;
+
+export const ListSeparator = styled.View`
+  height: ${({ theme }) => theme.space['spacing-12']};
+  background-color: transparent;
+`;
+
+export const ListHeaderContainer = styled.View`
+  margin-bottom: ${({ theme }) => theme.space['spacing-16']};
+  gap: ${({ theme }) => theme.space['spacing-12']};
+`;
+
+export const ListEmptyScreen = styled.View`
+  flex: 1;
+  align-items: center;
+`;
+
+export const RunewordsList = styled(FlatList).attrs<FlatListProps<IRunewords>>(
+  () => ({
+    contentContainerStyle: { padding: 16 },
+  }),
+)`
+  width: 100%;
+  margin-top: ${({ theme }) => theme.space['spacing-12']};
+`;
+
+export const FilterChip = styled.TouchableOpacity<{ selected: boolean }>`
+  width: 55%;
+  align-items: center;
+
+  padding-vertical: ${({ theme }) => theme.space['spacing-04']};
+  padding-horizontal: ${({ theme }) => theme.space['spacing-12']};
+  border-radius: ${({ theme }) => theme.radii['corner-48']};
+  background-color: ${({ theme: { colors }, selected }) =>
+    selected ? colors['primary-01'] : colors['surface-01']};
 `;
--- src/screens/ListRunes/ListRunes.tsx
+++ src/screens/ListRunes/ListRunes.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useReducer } from 'react';
 import * as S from './ListRunes.styles'
+import { MemoizedRunewordCard, SearchBar, Typography } from '@components';
+import { useRunewords } from '@hooks/useRunewords';
 
 export const ListRunes: React.FC = () => {
+  const [search, setSearch] = useReducer((_: string, value: string) => value, '')
+  const [listOnlyAvailable, setListOnlyAvaible] = useReducer((state: boolean) => !state, false)
+
+  const { runewords } = useRunewords({ search, listOnlyAvailable })
+
   return (
     <S.Container>
-      <Text>List Rune</Text>
+      <S.RunewordsList
+        ItemSeparatorComponent={S.ListSeparator}
+        ListHeaderComponent={
+          <S.ListHeaderContainer>
+            <SearchBar value={search} onChangeText={setSearch} />
+            <S.FilterChip selected={listOnlyAvailable} onPress={setListOnlyAvaible}>
+              <Typography variant='body-02'>Only Availables</Typography>
+            </S.FilterChip>
+          </S.ListHeaderContainer>
+        }
+        ListEmptyComponent={
+          <S.ListEmptyScreen>
+            <Typography color='surface-02'>No available runewords</Typography>
+          </S.ListEmptyScreen>
+        }
+        data={runewords}
+        renderItem={({ item }) => <MemoizedRunewordCard runeword={item} />} />
     </S.Container>
   );
 }
