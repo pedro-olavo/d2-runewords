512b37932076bf1ed74fb5fb2d761516365f9743
pedro-olavo <polavo00@gmail.com>
2024-06-17 00:23:46 -0300

    feat: creates Owned Runes screen

--- src/screens/OwnedRunes/OwnedRunes.styles.ts
+++ src/screens/OwnedRunes/OwnedRunes.styles.ts
@@ -1,6 +1,40 @@
+import { Runes } from '@stores/slices/types';
+import { FlatList, FlatListProps } from 'react-native';
 import styled from 'styled-components/native';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: ${({ theme }) => theme.colors['background-01']};
+  padding-horizontal: ${({ theme }) => theme.space['spacing-20']};
+`;
+
+export const RuneContainer = styled.TouchableOpacity<{ selected: boolean }>`
+  width: 25%;
+  align-items: center;
+  opacity: ${({ selected }) => (selected ? 1 : 0.5)};
+`;
+
+export const RuneImage = styled.Image`
+  width: 50px;
+  height: 50px;
+`;
+
+export const RuneTitleContainer = styled.View`
+  margin-top: ${({ theme }) => theme.space['spacing-04']};
+`;
+
+export const ListSeparator = styled.View`
+  height: ${({ theme }) => theme.space['spacing-12']};
+  background-color: transparent;
+`;
+
+export const ListHeaderContainer = styled.View`
+  margin-bottom: ${({ theme }) => theme.space['spacing-16']};
+`;
+
+export const RunesList = styled(FlatList).attrs<FlatListProps<Runes>>(() => ({
+  contentContainerStyle: { paddingVertical: 20 },
+}))`
+  width: 100%;
+  margin-top: ${({ theme }) => theme.space['spacing-12']};
 `;
--- src/screens/OwnedRunes/OwnedRunes.tsx
+++ src/screens/OwnedRunes/OwnedRunes.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import { Text, Touchable, TouchableOpacity } from 'react-native';
 import * as S from './OwnedRunes.styles'
-import { useUserRunes } from '@hooks/useUserRunes';
+import { Typography } from '@components';
+import { useRunes } from '@hooks/useRunes';
 
 export const OwnedRunes: React.FC = () => {
-
-  const { userRunes, handleAddRune } = useUserRunes()
-
-  console.log("🚀 ~ userRunes:", userRunes)
-
+  const { runes, toggleRuneOwnership } = useRunes()
 
   return (
     <S.Container>
-      {
-        userRunes.map((rune) => <Text style={{ color: 'white' }}>{rune}</Text>)
-      }
+      <S.RunesList
+        data={runes}
+        keyExtractor={(rune) => rune.name}
+        ListHeaderComponent={
+          <S.ListHeaderContainer>
+            <Typography variant='title-02' weight='bold' align='center'>Select Owned Runes</Typography>
+          </S.ListHeaderContainer>
+        }
+        renderItem={({ item }) => (
+          <S.RuneContainer selected={item.owned} onPress={() => toggleRuneOwnership(item.name)}>
+            <S.RuneImage source={item.image} />
+            <S.RuneTitleContainer>
+              <Typography>{item.name}</Typography>
+            </S.RuneTitleContainer>
+          </S.RuneContainer>
+        )}
+        ItemSeparatorComponent={S.ListSeparator}
+        numColumns={4}
 
-      <TouchableOpacity onPress={() => handleAddRune('El')}>
-        <Text style={{ color: 'white' }}>Add Rune</Text>
-      </TouchableOpacity>
+      />
     </S.Container >
   );
 }
